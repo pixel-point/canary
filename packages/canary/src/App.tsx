d5e6674f2da75348e2f6b10edafc69b224d45804
Tan Nhu <tnhu@users.noreply.github.com>
2024-06-27 13:14:34 -0700

    Clean up

--- packages/canary/src/App.tsx
+++ packages/canary/src/App.tsx
@@ -1,5 +1,8 @@
 import './App.css'
 import { Button } from '@/components/ui/button'
+import { RocketIcon, ReloadIcon, EnvelopeOpenIcon, ChevronRightIcon } from '@radix-ui/react-icons'
+
+import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 
 function App() {
   return (
@@ -11,6 +14,26 @@ function App() {
         <Button variant="ghost">Ghost</Button>
         <Button variant="link">Link</Button>
         <Button variant="outline">Outline</Button>
+        <Button variant="outline" size="icon">
+          <ChevronRightIcon className="h-4 w-4" />
+        </Button>
+        <Button>
+          <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
+        </Button>
+        <Button disabled>
+          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+          Please wait
+        </Button>
+      </div>
+
+      <div>
+        <div>
+          <Alert>
+            <RocketIcon className="h-4 w-4" />
+            <AlertTitle>Heads up!</AlertTitle>
+            <AlertDescription>You can add components to your app using the cli.</AlertDescription>
+          </Alert>
+        </div>
       </div>
     </div>
   )
