ab0ae243da7c513c47bcce1af5bdb13d48c34528
Tan Nhu <tnhu@users.noreply.github.com>
2024-07-10 15:03:58 -0700

    Add Playground app

---
+++ apps/playground/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ModeToggle } from './components/mode-toggle'
+import { ThemeProvider } from './components/theme-provider'
+import { LoginForm } from './pages/Login'
+
+export default function App() {
+  return (
+    <ThemeProvider defaultTheme="dark">
+      <main>
+        <LoginForm />
+        <ModeToggle />
+      </main>
+    </ThemeProvider>
+  )
+}
