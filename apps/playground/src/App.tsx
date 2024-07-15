c123e5e1c7fb3b0b4c29db11d787cb8eda3f60f4
Tan Nhu <tnhu@users.noreply.github.com>
2024-07-15 11:31:15 -0700

    Proper use of Canary styles.css in Playground

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,6 +1,7 @@
 import { ModeToggle } from './components/mode-toggle'
 import { ThemeProvider } from './components/theme-provider'
 import { LoginForm } from './pages/Login'
+import '@harnessio/canary/styles'
 
 export default function App() {
   return (
--- packages/canary/src/App.tsx
+++ packages/canary/src/App.tsx
@@ -1,41 +1,7 @@
-import { Button } from '@/components/button'
-import { RocketIcon, ReloadIcon, EnvelopeOpenIcon, ChevronRightIcon } from '@radix-ui/react-icons'
-
-import { Alert, AlertDescription, AlertTitle } from '@/components/alert'
+import React from 'react'
 
 function App() {
-  return (
-    <div style={{ display: 'flex', gap: '20px', flexDirection: 'column', padding: '100px' }}>
-      <div style={{ display: 'flex', gap: '20px', flexDirection: 'row' }}>
-        <Button>Button</Button>
-        <Button variant="secondary">Secondary</Button>
-        <Button variant="destructive">Destructive</Button>
-        <Button variant="ghost">Ghost</Button>
-        <Button variant="link">Link</Button>
-        <Button variant="outline">Outline</Button>
-        <Button variant="outline" size="icon">
-          <ChevronRightIcon className="h-4 w-4" />
-        </Button>
-        <Button>
-          <EnvelopeOpenIcon className="mr-2 h-4 w-4" /> Login with Email
-        </Button>
-        <Button disabled>
-          <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-          Please wait
-        </Button>
-      </div>
-
-      <div>
-        <div>
-          <Alert>
-            <RocketIcon className="h-4 w-4" />
-            <AlertTitle>Heads up!</AlertTitle>
-            <AlertDescription>You can add components to your app using the cli.</AlertDescription>
-          </Alert>
-        </div>
-      </div>
-    </div>
-  )
+  return <p>Use StoryBook instead...</p>
 }
 
 export default App
