ed8268aed0160a9a21004af9177d961fa61dc70d
Vardan Bansal <vardan.bansal@harness.io>
2024-07-17 17:10:38 -0700

    Cleanup webpack

--- packages/unified-pipeline/webpack.config.js
+++ packages/unified-pipeline/webpack.config.js
@@ -1,21 +1,21 @@
-const prod = process.env.NODE_ENV === "production";
+const prod = process.env.NODE_ENV === 'production'
 
-const path = require("path");
+const path = require('path')
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const webpack = require('webpack')
 
-const CONTEXT = process.cwd();
+const CONTEXT = process.cwd()
 
 module.exports = {
-  mode: prod ? "production" : "development",
-  entry: "./src/bootstrap.tsx",
+  mode: prod ? 'production' : 'development',
+  entry: './src/bootstrap.tsx',
   context: CONTEXT,
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -23,13 +23,13 @@ module.exports = {
         test: /\.(j|t)sx?$/,
         exclude: /node_modules/,
         resolve: {
-          extensions: [".ts", ".tsx", ".js", ".json"],
+          extensions: ['.ts', '.tsx', '.js', '.json']
         },
-        use: "ts-loader",
+        use: 'ts-loader'
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.module\.scss$/,
@@ -37,27 +37,27 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 1,
               modules: {
-                mode: "local",
-                localIdentName: "[name]_[local]",
-                exportLocalsConvention: "camelCaseOnly",
-              },
-            },
+                mode: 'local',
+                localIdentName: '[name]_[local]',
+                exportLocalsConvention: 'camelCaseOnly'
+              }
+            }
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sassOptions: {
-                includePaths: [path.join(CONTEXT, "src")],
+                includePaths: [path.join(CONTEXT, 'src')]
               },
               sourceMap: false,
-              implementation: require("sass"),
-            },
-          },
-        ],
+              implementation: require('sass')
+            }
+          }
+        ]
       },
       {
         test: /(?<!\.module)\.scss$/,
@@ -65,44 +65,39 @@ module.exports = {
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
+            loader: 'css-loader',
             options: {
               importLoaders: 1,
-              modules: false,
-            },
+              modules: false
+            }
           },
           {
-            loader: "sass-loader",
+            loader: 'sass-loader',
             options: {
               sassOptions: {
-                includePaths: [path.join(CONTEXT, "src")],
+                includePaths: [path.join(CONTEXT, 'src')]
               },
-              implementation: require("sass"),
-            },
-          },
-        ],
-      },
-    ],
+              implementation: require('sass')
+            }
+          }
+        ]
+      }
+    ]
   },
-  devtool: prod ? undefined : "cheap-module-source-map",
+  devtool: prod ? undefined : 'cheap-module-source-map',
   devServer: {
     hot: true,
     historyApiFallback: true,
     client: {
-      overlay: false,
-    },
+      overlay: false
+    }
   },
   plugins: [
     new HtmlWebpackPlugin({
-      publicPath: "/",
-      template: "index.html",
-      filename: "index.html",
+      publicPath: '/',
+      template: 'index.html',
+      filename: 'index.html'
     }),
-    new MiniCssExtractPlugin(),
-    new webpack.DefinePlugin({
-      "process.env.REACT_APP_SHOW_TOGGLE": JSON.stringify(
-        process.env.REACT_APP_SHOW_TOGGLE
-      ),
-    }),
-  ],
-};
+    new MiniCssExtractPlugin()
+  ]
+}
