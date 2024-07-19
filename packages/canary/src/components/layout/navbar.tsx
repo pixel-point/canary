94453dca7d7671092b16f258c25db7a6f171c82e
joetaylorland <accounts+github@joetaylor.net>
2024-07-19 14:58:59 +0100

    first pass at pixel matching left nav bar, componentize appshell

--- packages/canary/src/components/layout/navbar.tsx
+++ packages/canary/src/components/layout/navbar.tsx
@@ -1,78 +1,83 @@
 import companyAvatar from '@/assets/company-avatar.svg'
 import userAvatar from '@/assets/user-avatar.svg'
+import navItemPlaceholder from '@/assets/navbar-item-placeholder.svg'
+import navMore from '@/assets/navbar-more.svg'
 import React from 'react'
 import { cn } from '@/lib/utils'
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../accordion'
 
 const primaryMenuItems = [
   {
     text: 'Repositories',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   },
   {
     text: 'Pipelines',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   },
   {
     text: 'Executions',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   },
   {
     text: 'Featured flags',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   }
 ]
 
 const pinnedMenuItems = [
   {
     text: 'Chaos engineering',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   },
   {
     text: 'Environment',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   },
   {
     text: 'Secrets',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   },
   {
     text: 'Connectors',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   }
 ]
 
 const secondaryMenuItems = [
   {
     text: 'System administration',
-    iconSrc: companyAvatar
+    iconSrc: navItemPlaceholder
   }
 ]
 
-const SectionItem = React.memo(({ iconSrc, text, active }: { iconSrc: string; text: string; active?: boolean }) => {
-  return (
-    <div
-      className={cn('flex gap-2.5 items-center cursor-pointer group select-none py-1.5', { 'text-primary': active })}>
-      <img
-        src={iconSrc}
-        className={cn('navbar-section-icon opacity-50 ease-in-out duration-150', {
-          'opacity-100': active,
-          'group-hover:opacity-100': !active
-        })}
-      />
-      <p
-        className={cn('-tracking-[2%] ease-in-out duration-150 truncate', {
-          'text-primary': active,
-          'group-hover:text-primary': !active
-        })}>
-        {text}
-      </p>
-    </div>
-  )
-})
+const SectionItem = React.memo(
+  ({ iconSrc, text, active, value }: { iconSrc: string; text: string; active?: boolean; value?: string }) => {
+    return (
+      <div
+        className={cn('flex gap-2.5 items-center cursor-pointer group select-none py-1.5', { 'text-primary': active })}>
+        <img
+          src={iconSrc}
+          className={cn('navbar-section-icon opacity-100 ease-in-out duration-150', {
+            'opacity-100': active,
+            'group-hover:opacity-100': !active
+          })}
+        />
+        <p
+          className={cn('-tracking-[2%] ease-in-out duration-150 truncate', {
+            'text-primary': active,
+            'group-hover:text-primary': !active
+          })}>
+          {text}
+        </p>
+      </div>
+    )
+  }
+)
 
 const Company = React.memo(() => {
   return (
-    <div className="p-6 py-5 flex gap-2.5">
+    <div className="p-5 flex gap-2.5">
       <img src={companyAvatar} className="navbar-company-avatar" />
       <p className="text-[15px] text-primary">Pixel Point</p>
     </div>
@@ -81,29 +86,44 @@ const Company = React.memo(() => {
 
 const PrimaryList = React.memo(() => {
   return (
-    <div className="p-6 py-5 border-b border-[#18181B] flex flex-col gap-1.5">
+    <div className="p-5 pb-3.5 border-b border-[#18181B] flex flex-col gap-1.5">
       {primaryMenuItems.map((itm, itm_idx) => (
         <SectionItem key={itm_idx} text={itm.text} iconSrc={itm.iconSrc} active={itm_idx == 0} />
       ))}
-      <SectionItem text="More" iconSrc={companyAvatar} />
+      <SectionItem text="More" iconSrc={navMore} />
     </div>
   )
 })
 
 const PinnedList = React.memo(() => {
   return (
-    <div className="p-6 py-5 flex flex-col gap-1.5">
-      <p className="text-xs text-[#60606C] mb-1">Pinned</p>
-      {pinnedMenuItems.map((itm, itm_idx) => (
-        <SectionItem key={itm_idx} text={itm.text} iconSrc={itm.iconSrc} />
-      ))}
+    <div className="p-5 py-0.5">
+      <Accordion type="single" collapsible defaultValue="item-1">
+        <AccordionItem value="item-1" className="border-none">
+          <AccordionTrigger className="group">
+            <p className="text-xs text-[#60606C] font-light group-hover:text-primary ease-in-out duration-150">
+              Pinned
+            </p>
+          </AccordionTrigger>
+          <AccordionContent className="flex flex-col gap-1.5">
+            {pinnedMenuItems.map((itm, itm_idx) => (
+              <SectionItem
+                key={itm_idx}
+                value={'item-' + (itm_idx + 1).toString()}
+                text={itm.text}
+                iconSrc={itm.iconSrc}
+              />
+            ))}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   )
 })
 
 const SecondaryList = React.memo(() => {
   return (
-    <div className="p-6 py-5 border-t border-[#18181B] flex flex-col gap-1.5">
+    <div className="p-5 border-t border-[#18181B] flex flex-col gap-1.5">
       {secondaryMenuItems.map((itm, itm_idx) => (
         <SectionItem key={itm_idx} text={itm.text} iconSrc={itm.iconSrc} />
       ))}
@@ -117,7 +137,7 @@ const AIRegion = React.memo(() => {
 
 const User = React.memo(() => {
   return (
-    <div className="py-6 px-5 border-t border-[#18181B] grid grid-rows-2 grid-cols-[auto_1fr] gap-x-3 items-center justify-start cursor-pointer">
+    <div className="p-5 py-3.5 border-t border-[#18181B] grid grid-rows-2 grid-cols-[auto_1fr] gap-x-3 items-center justify-start cursor-pointer">
       <img src={userAvatar} className="col-start-1 row-span-2" />
       <p className="col-start-2 row-start-1 text-xs text-primary">Steven M.</p>
       <p className="col-start-2 row-start-2 text-xs font-light">Admin</p>
