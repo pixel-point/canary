f11e7b91abaf1fbfd9a204b7313c99bb0ae6f0d3
Vardan Bansal <vardan.bansal@harness.io>
2024-07-23 16:32:08 -0700

    harness icon integration - WIP

--- packages/canary/src/composites/footer/footer.tsx
+++ packages/canary/src/composites/footer/footer.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import moment from 'moment'
+/* Unable to import, get a lot of errors */
+// import { XmarkCircle } from '@harnessio/icons-noir'
 
 import css from './footer.module.scss'
 
@@ -29,6 +31,7 @@ const Footer: React.FC<FooterProps> = (props: FooterProps) => {
       <div className={css.flexContainer}>
         <div className={css.counts}>
           <div>
+            {/* <XmarkCircle /> */}
             <span className={css.focus}>{props.problems[Severity.ERROR]}</span>
           </div>
           <div>
