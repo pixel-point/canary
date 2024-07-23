4f7ff9d1247674ed64a8d3ba909137af085e9517
Vardan Bansal <vardan.bansal@harness.io>
2024-07-23 00:22:10 -0700

    Cleaning up mock json pipelines

--- packages/unified-pipeline/src/pages/studio/Studio.tsx
+++ packages/unified-pipeline/src/pages/studio/Studio.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { PipelineStudio } from "../../components/PipelineStudio/PipelineStudio";
-import { mockNodes as mockNodesDemo } from "../../assets/mockPipelines/mock_demo";
 import pipelineYamlSingleStage from "../../assets/mockPipelines/yamls/pipeline_w_single_stage.yaml";
 import pipelineYamlParallelGroup from "../../assets/mockPipelines/yamls/pipeline_w_parallel_stage_group.yaml";
 import pipelineYamlParallelGroupAndStage from "../../assets/mockPipelines/yamls/pipeline_w_parallel_stage_group_and_stage.yaml";
+import demoPipeline from "../../assets/mockPipelines/yamls/demo_pipeline.yaml";
 import { getGraphFromPipelineYAML } from "../../utils/PipelineYamlUtils";
 
 export const Studio: React.FC<{}> = () => {
@@ -45,7 +45,7 @@ export const StudioMixed: React.FC<{}> = () => {
 export const StudioDemo: React.FC<{}> = () => {
   return (
     <PipelineStudio
-      graph={{ nodes: mockNodesDemo as any }}
+      graph={getGraphFromPipelineYAML(demoPipeline)}
       onAddNode={() => {}}
       onDeleteNode={() => {}}
       onSelectNode={() => {}}
