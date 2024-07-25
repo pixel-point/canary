e2cdacb1c2de557be2261307d911b9639ddfa100
Vardan Bansal <vardan.bansal@harness.io>
2024-07-24 22:54:16 -0700

    Adding story

--- apps/storybook/src/stories/pages/Execution.stories.tsx
+++ apps/storybook/src/stories/pages/Execution.stories.tsx
@@ -1,6 +1,9 @@
+import React from 'react'
+import { Tree, Folder, File, Status } from '@harnessio/canary'
 import Container from '../../components/layout/container'
 import { GitnessNavbar } from '../components/NavBar.stories'
 import { GitnessTopBar } from '../components/TopBar.stories'
+import { elements } from '../../assets/mocks/mock'
 
 export default {
   title: 'Pages/Execution',
@@ -10,6 +13,41 @@ export default {
 }
 
 export function Execution() {
+  const TreeFileTest = () => {
+    return (
+      <div className="w-[395px] h-[660px]">
+        <Tree className="rounded-md bg-background overflow-hidden p-2" initialSelectedId="1" elements={elements}>
+          <Folder element="DAST" value="1" status={Status.SUCCESS} duration={15}>
+            <File value="2" status={Status.SUCCESS} duration={5}>
+              <p>Fortify</p>
+            </File>
+            <File value="3" status={Status.SUCCESS} duration={5}>
+              <p>Veracode</p>
+            </File>
+            <File value="4" status={Status.SUCCESS} duration={5}>
+              <p>Checkmarx</p>
+            </File>
+          </Folder>
+          <Folder element="Deploy to Prod" value="5" status={Status.FAILED}>
+            <Folder element="SBOM and SLSA Validation" value="6" status={Status.FAILED}>
+              <File value="9" status={Status.FAILED} duration={15}>
+                <p>SoftwareSupply Chain Validation</p>
+              </File>
+              <File value="10" status={Status.FAILED}>
+                <p>SLSA Verification</p>
+              </File>
+            </Folder>
+            <File value="7" status={Status.QUEUED}>
+              <p>Risk Profile OPA - New Criticals</p>
+            </File>
+            <File value="8" status={Status.QUEUED}>
+              <p>Canary Deployment</p>
+            </File>
+          </Folder>
+        </Tree>
+      </div>
+    )
+  }
   return (
     <Container.Root>
       <Container.Sidebar>
@@ -19,9 +57,9 @@ export function Execution() {
         <Container.Topbar>
           <GitnessTopBar />
         </Container.Topbar>
-        <Container.CenteredContent>
-          <p className="text-sm">Execution – Single</p>
-        </Container.CenteredContent>
+        <Container.Content>
+          <TreeFileTest />
+        </Container.Content>
       </Container.Main>
     </Container.Root>
   )
