c4e9566080702da9887df82050e21a0958d9e3c0
Vardan Bansal <vardan.bansal@harness.io>
2024-07-25 00:19:29 -0700

    Updating demo pipeline

--- packages/unified-pipeline/src/pages/studio/Studio.tsx
+++ packages/unified-pipeline/src/pages/studio/Studio.tsx
@@ -3,9 +3,9 @@ import { Node as ReactFlowNode } from 'reactflow'
 import { PipelineStudio } from '../../components/PipelineStudio/PipelineStudio'
 import pipelineYamlSingleStage from '../../assets/mockPipelines/yamls/pipeline_w_single_stage.yaml'
 import pipelineYamlParallelGroup from '../../assets/mockPipelines/yamls/pipeline_w_parallel_stage_group.yaml'
-import pipelineYamlSequentialGroup from '../../assets/mockPipelines/yamls/pipeline_w_sequential_stage_group.yaml'
+// import pipelineYamlSequentialGroup from '../../assets/mockPipelines/yamls/pipeline_w_sequential_stage_group.yaml'
 import pipelineYamlParallelGroupAndStage from '../../assets/mockPipelines/yamls/pipeline_w_parallel_stage_group_and_stage.yaml'
-// import demoPipeline from '../../assets/mockPipelines/yamls/demo_pipeline.yaml'
+import demoPipeline from '../../assets/mockPipelines/yamls/demo_pipeline.yaml'
 import { getNodesFromPipelineYaml } from '../../utils/PipelineYamlUtils'
 
 export const Studio: React.FC<{}> = () => {
@@ -44,7 +44,7 @@ export const StudioMixed: React.FC<{}> = () => {
 export const StudioDemo: React.FC<{}> = () => {
   return (
     <PipelineStudio
-      nodes={getNodesFromPipelineYaml(pipelineYamlSequentialGroup)}
+      nodes={getNodesFromPipelineYaml(demoPipeline)}
       onAddNode={() => {}}
       onDeleteNode={() => {}}
       onSelectNode={() => {}}
