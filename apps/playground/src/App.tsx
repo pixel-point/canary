35b99470775b98ab5c25fc7f10abcdf0349516bf
Vardan Bansal <vardan.bansal@harness.io>
2024-08-06 01:53:45 -0700

    minor revert

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
 import { ModeToggle } from './components/mode-toggle'
 import { ThemeProvider } from './components/theme-provider'
-import { Execution } from './pages/Execution'
 import { LoginForm } from './pages/Login'
 import '@harnessio/canary/styles'
 
@@ -9,9 +7,8 @@ export default function App() {
   return (
     <ThemeProvider defaultTheme="dark">
       <main>
-        {/* <LoginForm />
-        <ModeToggle /> */}
-        <Execution />
+        <LoginForm />
+        <ModeToggle />
       </main>
     </ThemeProvider>
   )
