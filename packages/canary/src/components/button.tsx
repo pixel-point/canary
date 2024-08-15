3e2e315bf2dee4576512f83a2793eabe81cad094
Abhinav Rastogi <abhinav.rastogi@harness.io>
2024-08-15 13:20:19 -0700

    fix: remove hack

--- packages/canary/src/components/button.tsx
+++ packages/canary/src/components/button.tsx
@@ -4,7 +4,6 @@ import { cva, type VariantProps } from 'class-variance-authority'
 
 import { cn } from '@/lib/utils'
 import { CanaryOutletFactory, CanaryOutletName } from '@/lib/CanaryOutletFactory'
-import { Icon } from './icon'
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
@@ -90,7 +89,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     ) : (
       children
     )
-    return <Icon />
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, padding, borderRadius, className }))}
