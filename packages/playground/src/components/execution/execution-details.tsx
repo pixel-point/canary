ade55a1687593cee66a9d5ac015416580659866c
Vardan Bansal <vardan.bansal@harness.io>
2024-08-18 15:19:29 -0700

    Wiring up step nav

--- packages/playground/src/components/execution/execution-details.tsx
+++ packages/playground/src/components/execution/execution-details.tsx
@@ -2,14 +2,27 @@ import React from 'react'
 import { Text } from '@harnessio/canary'
 import { StageExecution } from './stage-execution'
 import { data as mockExecution } from './mocks/mockExecution'
+import { Layout } from '../layout/layout'
+import { ExecutionTree } from './execution-tree'
+import { elements } from '../../assets/mockExecutionTree'
 
 interface ExecutionProps {
   pipelineId: unknown
   executionId: unknown
 }
 
-export const ExecutionDetails: React.FC<ExecutionProps> = (_props): React.ReactElement => {
+export const ExecutionDetails: React.FC<ExecutionProps> = (): React.ReactElement => {
   const stages = mockExecution.stages
   if (!stages || !stages.length) return <Text>No stages found</Text>
-  return <StageExecution stage={stages[0]} />
+  return (
+    <Layout.Horizontal>
+      {/* Hardcoded height added temporarily */}
+      <div className="h-[calc(100vh-16rem)] overflow-y-scroll">
+        <StageExecution stage={stages[0]} />
+      </div>
+      <div className="w-[450px] h-[calc(100vh-16rem)] overflow-y-scroll">
+        <ExecutionTree defaultSelectedId="2" elements={elements} onSelectNode={() => {}} />
+      </div>
+    </Layout.Horizontal>
+  )
 }
--- packages/playground/src/components/execution/step-execution.tsx
+++ packages/playground/src/components/execution/step-execution.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@harnessio/canary'
+import { Tabs, TabsContent, TabsList, TabsTrigger, Input, Button, Text } from '@harnessio/canary'
+import { Copy, Edit, Download } from '@harnessio/icons-noir'
 import ConsoleLogs from './console-logs'
 import { data } from './mocks/mockStepLogs'
 import { Layout } from '../layout/layout'
-import { ExecutionTree } from './execution-tree'
-import { elements } from '../../assets/mockExecutionTree'
 
 export interface StepProps {
   name: string
@@ -12,6 +11,7 @@ export interface StepProps {
 
 interface StageExecutionProps {
   step: StepProps
+  stepIndex: number
 }
 
 enum StepExecutionTab {
@@ -20,29 +20,48 @@ enum StepExecutionTab {
   OUTPUT = 'output'
 }
 
-export const StepExecution: React.FC<StageExecutionProps> = (props): React.ReactElement => {
+const StepExecutionToolbar: React.FC = () => {
   return (
-    <Tabs defaultValue={StepExecutionTab.LOG} className="w-full h-full mt-2">
-      <Layout.Vertical gap="space-y-3">
-        <TabsList className="w-fit">
-          <TabsTrigger className="h-full" value={StepExecutionTab.LOG}>
-            Logs
-          </TabsTrigger>
-          <TabsTrigger value={StepExecutionTab.INPUT}>Inputs</TabsTrigger>
-          <TabsTrigger value={StepExecutionTab.OUTPUT}>Output</TabsTrigger>
-        </TabsList>
-        <TabsContent value={StepExecutionTab.LOG}>
-          <Layout.Horizontal>
-            {/* Hardcoded value added temporarily */}
-            <div className="border-t pt-4 h-[calc(100vh-20.5rem)] overflow-y-scroll">
-              <ConsoleLogs logs={data[0]} />
-            </div>
-            <div className="w-[450px] h-[calc(100vh-20.5rem)] overflow-y-scroll">
-              <ExecutionTree defaultSelectedId="2" elements={elements} onSelectNode={() => {}} />
-            </div>
+    <Layout.Horizontal>
+      <Input placeholder="Find in logs" />
+      <div className="flex">
+        <Button variant="outline" size="icon" className="rounded-tr-none rounded-br-none border-r-0">
+          <Copy className="h-4 w-4" />
+        </Button>
+        <Button variant="outline" size="icon" className="rounded-none">
+          <Edit className="h-4 w-4" />
+        </Button>
+        <Button variant="outline" size="icon" className="rounded-tl-none rounded-bl-none border-l-0">
+          <Download className="h-4 w-4" />
+        </Button>
+      </div>
+    </Layout.Horizontal>
+  )
+}
+
+export const StepExecution: React.FC<StageExecutionProps> = ({ step, stepIndex }) => {
+  return (
+    <Layout.Vertical>
+      <Text>{step.name}</Text>
+      <Tabs defaultValue={StepExecutionTab.LOG} className="w-full h-full mt-2">
+        <Layout.Vertical gap="space-y-3">
+          <Layout.Horizontal className="flex justify-between">
+            <TabsList className="w-fit">
+              <TabsTrigger className="h-full" value={StepExecutionTab.LOG}>
+                Logs
+              </TabsTrigger>
+              <TabsTrigger value={StepExecutionTab.INPUT}>Inputs</TabsTrigger>
+              <TabsTrigger value={StepExecutionTab.OUTPUT}>Output</TabsTrigger>
+            </TabsList>
+            <StepExecutionToolbar />
           </Layout.Horizontal>
-        </TabsContent>
-      </Layout.Vertical>
-    </Tabs>
+          <TabsContent value={StepExecutionTab.LOG}>
+            <div className="border-t pt-4">
+              <ConsoleLogs logs={data[stepIndex]} />
+            </div>
+          </TabsContent>
+        </Layout.Vertical>
+      </Tabs>
+    </Layout.Vertical>
   )
 }
