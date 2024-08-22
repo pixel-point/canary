b239fc7e0d12fbd5a49379228d4f4eb5c1159a56
srdjan-harness <69592850+srdjan-harness@users.noreply.github.com>
2024-08-23 00:13:46 +0200

    Extend input component (#120)
    * extend input component
    
    * fix

--- apps/storybook/src/stories/components/Input.stories.tsx
+++ apps/storybook/src/stories/components/Input.stories.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import type { Meta, StoryObj } from '@storybook/react'
-import { Input, InputProps } from '../../../../../packages/canary/src/components/input'
+import { Input, InputProps } from '@harnessio/canary'
 import { Search, WarningTriangle } from '@harnessio/icons-noir'
 
 const meta: Meta<InputProps> = {
   title: 'Components/Input',
   component: Input,
   parameters: {
+    layout: 'centered',
     docs: {
       description: {
         component: `Input component.`
@@ -15,10 +16,12 @@ const meta: Meta<InputProps> = {
   },
   tags: ['autodocs'],
   argTypes: {
-    type: { control: 'select', options: ['text', 'password'] }
+    type: { control: 'select', options: ['text', 'password'] },
+    placeholder: { control: 'text' }
   },
   args: {
-    type: 'text'
+    type: 'text',
+    placeholder: 'Placeholder ...'
   }
 }
 
@@ -30,26 +33,66 @@ export const Primary: StoryObj<InputProps> = {
 
 export const LeftIcon: StoryObj<InputProps> = {
   args: {
-    leftIcon: <Search />
+    left: <Search />
   }
 }
 
 export const RightIcon: StoryObj<InputProps> = {
   args: {
-    rightIcon: <WarningTriangle />
+    right: <WarningTriangle />
   }
 }
 
 export const LeftRightIcon: StoryObj<InputProps> = {
   args: {
-    leftIcon: <Search />,
-    rightIcon: <WarningTriangle />
+    left: <Search />,
+    right: <WarningTriangle />
   }
 }
 
 export const IconsCustomSize: StoryObj<InputProps> = {
   args: {
-    leftIcon: <Search size="20" />,
-    rightIcon: <WarningTriangle size="20" />
+    left: <Search size="25" />,
+    right: <WarningTriangle size="25" />
+  }
+}
+
+export const LeftIconStyled: StoryObj<InputProps> = {
+  args: {
+    left: <Search />,
+    leftStyle: true
+  }
+}
+
+export const RightIconStyled: StoryObj<InputProps> = {
+  args: {
+    right: <WarningTriangle />
+  }
+}
+
+export const LeftRightIconStyled: StoryObj<InputProps> = {
+  args: {
+    left: <Search />,
+    leftStyle: true,
+    right: <WarningTriangle />,
+    rightStyle: true
+  }
+}
+
+export const IconsCustomSizeStyled: StoryObj<InputProps> = {
+  args: {
+    left: <Search size="25" />,
+    leftStyle: true,
+    right: <WarningTriangle size="25" />,
+    rightStyle: true
+  }
+}
+
+export const TextStyled: StoryObj<InputProps> = {
+  args: {
+    left: 'https://',
+    leftStyle: true,
+    right: '.io',
+    rightStyle: true
   }
 }
--- packages/canary/src/components/input.tsx
+++ packages/canary/src/components/input.tsx
@@ -1,50 +1,74 @@
 import * as React from 'react'
-import { cn } from '../lib/utils'
+import { cn } from '@/lib/utils'
 
-export interface BaseInputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface BaseInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  isExtended?: boolean
+}
 
-const BaseInput = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      type={type}
-      className={cn(
-        'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  )
+const BaseInput = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, isExtended, ...props }, ref) => {
+  const commonClassName = 'bg-transparent px-3 py-1'
+  const specificClassNames = isExtended
+    ? 'border-none grow focus-visible:outline-none'
+    : 'flex h-9 w-full rounded-md border border-input text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
+
+  return <input type={type} className={cn(commonClassName, specificClassNames, className)} ref={ref} {...props} />
 })
 
 export interface ExtendedInputProps extends BaseInputProps {
-  leftIcon?: React.ReactElement
-  rightIcon?: React.ReactElement
+  left?: React.ReactNode
+  leftStyle?: boolean
+  leftClassName?: string
+  right?: React.ReactNode
+  rightStyle?: boolean
+  rightClassName?: string
 }
 
 const ExtendedInput = React.forwardRef<HTMLInputElement, ExtendedInputProps>(
-  ({ className, type, leftIcon, rightIcon, ...props }, ref) => {
-    const leftClassName = leftIcon ? 'pl-8' : ''
-    const rightClassName = rightIcon ? 'pr-8' : ''
+  ({ className, type, left, leftStyle, leftClassName, right, rightStyle, rightClassName, ...props }, ref) => {
+    const containerClassName =
+      'flex h-9 w-full rounded-md border border-input text-sm shadow-sm transition-colors placeholder:text-muted-foreground focus-within:outline-none focus-within:ring-1 focus-within:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
+
+    const leftRightCommonClassName = 'flex items-center px-3 text-muted-foreground'
 
     return (
-      <div className="relative flex-grow">
-        {leftIcon ? <div className="absolute inset-y-0 left-0 flex items-center pl-2">{leftIcon}</div> : null}
-        <BaseInput className={cn(className, leftClassName, rightClassName)} type={type} {...props} ref={ref} />
-        {rightIcon ? <div className="absolute inset-y-0 right-0 flex items-center pr-2">{rightIcon} </div> : null}
+      <div className={cn(containerClassName, className)}>
+        {left && (
+          <div
+            className={cn(
+              leftRightCommonClassName,
+              'rounded-l-md',
+              leftStyle ? 'border-r bg-muted' : '-mr-3',
+              leftClassName
+            )}>
+            {left}
+          </div>
+        )}
+        <BaseInput className={className} type={type} {...props} ref={ref} isExtended={true} />
+        {right && (
+          <div
+            className={cn(
+              leftRightCommonClassName,
+              'rounded-r-md',
+              rightStyle ? 'border-l bg-muted' : '-ml-3',
+              rightClassName
+            )}>
+            {right}
+          </div>
+        )}
       </div>
     )
   }
 )
 
-export interface InputProps extends BaseInputProps, ExtendedInputProps {}
+interface InputProps extends Omit<BaseInputProps, 'isExtended'>, ExtendedInputProps {}
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ leftIcon, rightIcon, ...props }, ref) => {
-  if (leftIcon || rightIcon) {
-    return <ExtendedInput leftIcon={leftIcon} rightIcon={rightIcon} {...props} ref={ref} />
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ left, right, ...props }, ref) => {
+  if (left || right) {
+    return <ExtendedInput left={left} right={right} {...props} ref={ref} />
   }
   return <BaseInput {...props} ref={ref} />
 })
 Input.displayName = 'Input'
 
 export { Input }
+export type { InputProps }
