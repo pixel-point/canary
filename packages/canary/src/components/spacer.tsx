13bb244f34c7f3084a7766aacbb2fe076b897fdb
Alex Zemlyakov <zemka4@gmail.com>
2024-11-05 23:08:14 +0800

    fix: repository list

--- packages/canary/src/components/spacer.tsx
+++ packages/canary/src/components/spacer.tsx
@@ -3,7 +3,7 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 import { cva, type VariantProps } from 'class-variance-authority'
 
-const spacerVariants = cva('mt-4 block pb-px', {
+const spacerVariants = cva('block mt-4', {
   variants: {
     size: {
       1: 'mt-1',
