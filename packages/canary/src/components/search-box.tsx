8bc92a591b695a24ca7e4388afc47ffd52a51628
Alex Zemlyakov <zemka4@gmail.com>
2024-11-11 19:09:56 +0800

    feat: added new colors system

--- packages/canary/src/components/search-box.tsx
+++ packages/canary/src/components/search-box.tsx
@@ -116,7 +116,7 @@ const Root = ({
 
         // Start of temporary fix
         defaultValue={defaultValue}
-        className={cn('border-input-foreground h-8 pt-[0.4375rem]', textSizeClass)}
+        className={cn('h-8 pt-[0.4375rem]', textSizeClass)}
         style={{
           paddingLeft: '1.75rem', // Equivalent to 'pl-7' in Tailwind (28px)
           paddingRight: hasShortcut ? '2.5rem' : undefined // Equivalent to 'pr-10' in Tailwind (40px) if `hasShortcut` is true
