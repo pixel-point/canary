06bb68a36c50ecd1279b722f18eb32cb50097005
Andrew Golovanov <spacewebdeveloper@gmail.com>
2024-11-12 02:23:16 +0100

    feat: add BranchInfoBar component

--- packages/canary/src/components/button.tsx
+++ packages/canary/src/components/button.tsx
@@ -9,9 +9,10 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        default: 'bg-primary text-foreground-6 shadow hover:bg-primary/90',
         destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-        outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+        outline:
+          'border border-borders-2 bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground text-foreground-2',
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         tertiary: 'bg-tertiary text-secondary-foreground shadow-sm hover:bg-tertiary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
