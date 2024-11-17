80aabfaec50ab9375c1a97ee55ab071e4f352314
Andrew Golovanov <spacewebdeveloper@gmail.com>
2024-11-17 01:02:05 +0100

    feat: add filtering for calendar is_between

--- packages/playground/src/components/filters/filters-bar/filter-variants/calendar.tsx
+++ packages/playground/src/components/filters/filters-bar/filter-variants/calendar.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Calendar as UICalendar } from '@harnessio/canary'
+import { Calendar as UICalendar, type CalendarDateRange } from '@harnessio/canary'
 import { FilterValue } from '../../types'
 
 interface CalendarProps {
@@ -12,16 +12,48 @@ const Calendar = ({ filter, onUpdateFilter }: CalendarProps) => {
     filter.selectedValues[0] ? new Date(filter.selectedValues[0]) : undefined
   )
 
+  const [dateRange, setDateRange] = useState<CalendarDateRange | undefined>(
+    filter.selectedValues.length === 2
+      ? {
+          from: new Date(filter.selectedValues[0]),
+          to: new Date(filter.selectedValues[1])
+        }
+      : undefined
+  )
+
+  const isBetweenMode = filter.condition === 'is_between'
+
+  if (isBetweenMode) {
+    return (
+      <div className="mt-2.5 px-2 pb-3 pt-1">
+        <UICalendar
+          className="p-0"
+          mode="range"
+          selected={dateRange}
+          onSelect={(value: CalendarDateRange | undefined) => {
+            if (value) {
+              setDateRange(value)
+              if (value.from && value.to) {
+                onUpdateFilter(filter.type, [value.from.toISOString(), value.to.toISOString()])
+              }
+            }
+          }}
+          initialFocus
+        />
+      </div>
+    )
+  }
+
   return (
     <div className="mt-2.5 px-2 pb-3 pt-1">
       <UICalendar
         className="p-0"
         mode="single"
         selected={date}
-        onSelect={newDate => {
-          setDate(newDate)
-          if (newDate) {
-            onUpdateFilter(filter.type, [newDate.toISOString()])
+        onSelect={(value: Date | undefined) => {
+          if (value) {
+            setDate(value)
+            onUpdateFilter(filter.type, [value.toISOString()])
           }
         }}
         initialFocus
