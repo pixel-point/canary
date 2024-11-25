eab93fad80d258cfb2d978804e817f12f16a7c87
Alex Zemlyakov <zemka4@gmail.com>
2024-11-25 21:59:17 +0800

    feat: commit for stand

--- packages/playground/tailwind.config.js
+++ packages/playground/tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [require('@harnessio/canary/tailwind.config'), require('@harnessio/unified-pipeline/tailwind.config')],
+  presets: [
+    require('@harnessio/canary/tailwind.config'),
+    require('@harnessio/ui/tailwind.config'),
+    require('@harnessio/unified-pipeline/tailwind.config')
+  ],
   content: [
     './src/**/*.{ts,tsx}',
     'node_modules/@harnessio/canary/src/**/*.{ts,tsx}',
-    'node_modules/@harnessio/unified-pipeline/src/**/*.{ts,tsx}'
+    'node_modules/@harnessio/unified-pipeline/src/**/*.{ts,tsx}',
+    'node_modules/@harnessio/ui/src/**/*.{ts,tsx}',
   ],
   theme: {
     extend: {
