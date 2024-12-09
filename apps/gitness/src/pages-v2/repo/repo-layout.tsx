1cc045bc76fa18b3c7aa4cfa3dac9b97e8f6b1d4
vivek-harness <c_vivek.patel@harness.io>
2024-12-09 15:34:05 -0800

    Fixed Settings Menu Overlap with Breadcrumbs (#551)

--- apps/gitness/src/pages-v2/repo/repo-layout.tsx
+++ apps/gitness/src/pages-v2/repo/repo-layout.tsx
@@ -6,7 +6,7 @@ import { useTranslationStore } from '../../i18n/stores/i18n-store'
 const RepoLayout = () => {
   return (
     <>
-      <div className="fixed top-0 z-50 ml-56 w-full bg-background-1">
+      <div className="fixed top-0 z-30 ml-56 w-full bg-background-1">
         <Breadcrumbs />
       </div>
       <RepoLayoutView useTranslationStore={useTranslationStore} />
