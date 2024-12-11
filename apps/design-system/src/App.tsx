562ce0a0b2be4e56f6e7e8f890058e2f619983cf
Kevin Nagurski <Kevin.nagurski@harness.io>
2024-12-11 21:29:40 +0000

    chore: Redirect the root of the design system app to  temporarily (#568)

--- apps/design-system/src/App.tsx
+++ apps/design-system/src/App.tsx
@@ -1,9 +1,12 @@
 import { FC } from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 
 import ViewPreview from './pages/view-preview/view-preview.tsx'
 
-const router = createBrowserRouter([{ path: '/view-preview/*', element: <ViewPreview /> }])
+const router = createBrowserRouter([
+  { path: '/view-preview/*', element: <ViewPreview /> },
+  { path: '/*', element: <Navigate to="/view-preview" /> } // temp redirect to view preview
+])
 
 const App: FC = () => {
   return <RouterProvider router={router} />
