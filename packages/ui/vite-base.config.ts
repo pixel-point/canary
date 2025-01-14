4d866278a3aa27b265041a274de110b930578b6e
Kevin Nagurski <Kevin.nagurski@harness.io>
2025-01-14 12:57:47 +0000

    chore: build:ci script to speed up production build for design-system app (#662)

---
+++ packages/ui/vite-base.config.ts
@@ -0,0 +1,37 @@
+import { resolve } from 'path'
+
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import svgr from 'vite-plugin-svgr'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+const external = [
+  'react',
+  'react-hook-form',
+  'react-router-dom',
+  'react-router',
+  'react/jsx-runtime',
+  'react-i18next',
+  'i18next'
+]
+
+export default defineConfig({
+  plugins: [react(), svgr({ include: '**/*.svg' }), tsConfigPaths()],
+  build: {
+    lib: {
+      cssFileName: 'styles',
+      entry: {
+        components: resolve(__dirname, 'src/components/index.ts'),
+        views: resolve(__dirname, 'src/views/index.ts'),
+        hooks: resolve(__dirname, 'src/hooks/index.ts'),
+        locales: resolve(__dirname, 'locales/index.ts'),
+        index: resolve(__dirname, 'src/index.ts')
+      },
+      formats: ['es']
+    },
+    rollupOptions: {
+      external: external
+    },
+    sourcemap: true
+  }
+})
