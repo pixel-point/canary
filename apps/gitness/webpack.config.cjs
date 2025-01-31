6e787162de80f6f3d363f31234fda9395d3751ad
praneshg239 <95267551+praneshg239@users.noreply.github.com>
2025-01-31 23:14:54 +0530

    Reduce the monaco-editor version to 0.40.0 to match the version on NGUI (#904)
    * Reduce the monaco-editor version to 0.40.0 to match the version on NGUI
    
    * Add monaco-yaml to gitness
    
    * downgrade monaco-editor to 0.40.0
    
    * update pnpm-lock.yaml
    
    * add caret to semver
    
    * update version; update pnpm-lock.yaml
    
    ---------
    
    Co-authored-by: Srdjan Arsic <srdjan.arsic@harness.io>

--- apps/gitness/webpack.config.cjs
+++ apps/gitness/webpack.config.cjs
@@ -1,4 +1,5 @@
 const { container } = require('webpack')
+const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 
 const { ModuleFederationPlugin } = container
 
@@ -19,8 +20,8 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
     alias: {
-      "react/jsx-dev-runtime": "react/jsx-dev-runtime.js",
-      "react/jsx-runtime": "react/jsx-runtime.js"
+      'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+      'react/jsx-runtime': 'react/jsx-runtime.js'
     }
   },
   module: {
@@ -57,6 +58,103 @@ module.exports = {
           eager: true
         }
       }
+    }),
+    new MonacoWebpackPlugin({
+      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+      languages: [
+        'abap',
+        'apex',
+        'azcli',
+        'bat',
+        'bicep',
+        'cameligo',
+        'clojure',
+        'coffee',
+        'cpp',
+        'csharp',
+        'csp',
+        'css',
+        'cypher',
+        'dart',
+        'dockerfile',
+        'ecl',
+        'elixir',
+        'flow9',
+        'freemarker2',
+        'fsharp',
+        'go',
+        'graphql',
+        'handlebars',
+        'hcl',
+        'html',
+        'ini',
+        'java',
+        'javascript',
+        'json',
+        'julia',
+        'kotlin',
+        'less',
+        'lexon',
+        'liquid',
+        'lua',
+        'm3',
+        'markdown',
+        'mips',
+        'msdax',
+        'mysql',
+        'objective-c',
+        'pascal',
+        'pascaligo',
+        'perl',
+        'pgsql',
+        'php',
+        'pla',
+        'postiats',
+        'powerquery',
+        'powershell',
+        'protobuf',
+        'pug',
+        'python',
+        'qsharp',
+        'r',
+        'razor',
+        'redis',
+        'redshift',
+        'restructuredtext',
+        'ruby',
+        'rust',
+        'sb',
+        'scala',
+        'scheme',
+        'scss',
+        'shell',
+        'solidity',
+        'sophia',
+        'sparql',
+        'sql',
+        'st',
+        'swift',
+        'systemverilog',
+        'tcl',
+        'twig',
+        'typescript',
+        'vb',
+        'wgsl',
+        'xml',
+        'yaml'
+      ],
+      globalAPI: true,
+      filename: '[name].worker.[contenthash:6].js',
+      customLanguages: [
+        {
+          label: 'yaml',
+          entry: 'monaco-yaml',
+          worker: {
+            id: 'monaco-yaml/yamlWorker',
+            entry: 'monaco-yaml/yaml.worker'
+          }
+        }
+      ]
     })
   ]
 }
