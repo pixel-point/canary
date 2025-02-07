4d3bbf4be39e0c9a7e172059e080dbb19f260988
Sanskar <c_sanskar.sehgal@harness.io>
2025-02-07 02:11:44 -0800

    Tab nav style fixes (#1012)
    * chore: fix tabnav styles
    
    * chore: clean up
    
    * fix: review comments

--- packages/ui/src/components/tabnav.tsx
+++ packages/ui/src/components/tabnav.tsx
@@ -1,5 +1,7 @@
 import { NavLink, NavLinkProps } from 'react-router-dom'
 
+import { cn } from '@utils/cn'
+
 const TabNavRoot: React.FC = ({ children }) => {
   return (
     <nav className="inline-flex h-[44px] w-full items-center justify-start gap-6 border-b border-border-background px-6 text-muted-foreground">
@@ -8,13 +10,18 @@ const TabNavRoot: React.FC = ({ children }) => {
   )
 }
 
-const commonClasses = 'flex h-full items-center text-center cursor-pointer'
+const commonClasses =
+  'flex h-full items-center text-center cursor-pointer border-solid border-b-2 border-b-transparent px-0 font-normal text-foreground-2 duration-150 ease-in-out hover:text-foreground-1'
 
 const TabNavItem: React.FC<NavLinkProps> = ({ children, ...props }) => {
   return (
     <NavLink
       role="tab"
-      className={({ isActive }) => (isActive ? `${commonClasses} text-primary border-b border-primary` : commonClasses)}
+      className={({ isActive }) =>
+        cn(commonClasses, {
+          'text-primary border-borders-9': isActive
+        })
+      }
       {...props}
     >
       {children}
