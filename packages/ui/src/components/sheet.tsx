f36f721b60ce238e03f30bd21649873188d34ba1
Vardan Bansal <vardan.bansal@harness.io>
2025-03-24 17:38:48 -0700

    Fix scroll issue (#1292)

--- packages/ui/src/components/sheet.tsx
+++ packages/ui/src/components/sheet.tsx
@@ -87,28 +87,29 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
     const { portalContainer } = usePortal()
     return (
       <SheetPortal container={portalContainer}>
-        <SheetOverlay modal={modal} className={overlayClassName} handleClose={handleClose || props.onClick} />
-        <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
-          {children}
-          {!hideCloseButton && (
-            <SheetPrimitive.Close
-              className="absolute right-[0.1875rem] top-2 flex items-center justify-center transition-colors disabled:pointer-events-none"
-              asChild
-            >
-              <Button
-                className={cn(
-                  'text-icons-4 hover:text-icons-2 focus:ring-0 focus-visible:outline-none',
-                  closeClassName
-                )}
-                variant="custom"
-                size="icon"
+        <SheetOverlay modal={modal} className={overlayClassName} handleClose={handleClose || props.onClick}>
+          <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
+            {children}
+            {!hideCloseButton && (
+              <SheetPrimitive.Close
+                className="absolute right-[0.1875rem] top-2 flex items-center justify-center transition-colors disabled:pointer-events-none"
+                asChild
               >
-                <Icon name="close" size={16} />
-                <span className="sr-only">Close</span>
-              </Button>
-            </SheetPrimitive.Close>
-          )}
-        </SheetPrimitive.Content>
+                <Button
+                  className={cn(
+                    'text-icons-4 hover:text-icons-2 focus:ring-0 focus-visible:outline-none',
+                    closeClassName
+                  )}
+                  variant="custom"
+                  size="icon"
+                >
+                  <Icon name="close" size={16} />
+                  <span className="sr-only">Close</span>
+                </Button>
+              </SheetPrimitive.Close>
+            )}
+          </SheetPrimitive.Content>
+        </SheetOverlay>
       </SheetPortal>
     )
   }
