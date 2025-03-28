9b2b65f9c2c227ca9a42f34feee79972cdeb71f7
Vardan Bansal <vardan.bansal@harness.io>
2025-03-28 14:43:16 -0700

    Fix button group css (#1338)
    * fix css
    
    * fix middle btn as well
    
    * minor revert

--- packages/ui/src/components/file-toolbar-actions.tsx
+++ packages/ui/src/components/file-toolbar-actions.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 
 import { Button, ButtonGroup, CopyButton, Icon } from '@/components'
 
-const buttonClassName = 'rounded-none border-l border-borders-1 hover:bg-background-4'
+const buttonClassName = 'rounded-none border-borders-1 hover:bg-background-4'
 
 export interface FileToolbarActionsProps {
   onDownloadClick: () => void
@@ -21,12 +21,18 @@ export const FileToolbarActions: FC<FileToolbarActionsProps> = ({
     <ButtonGroup spacing="0" className="shadow-borders-1 h-full rounded shadow-as-border">
       <CopyButton className={`${buttonClassName} rounded-l`} name={copyContent} />
       {showEdit && (
-        <Button className={buttonClassName} size="icon" variant="custom" aria-label="Edit" onClick={onEditClick}>
+        <Button
+          className={`${buttonClassName} border-l`}
+          size="icon"
+          variant="custom"
+          aria-label="Edit"
+          onClick={onEditClick}
+        >
           <Icon name="edit-pen" size={16} className="text-icons-3" />
         </Button>
       )}
       <Button
-        className={`${buttonClassName} rounded-r`}
+        className={`${buttonClassName} border-l rounded-r`}
         size="icon"
         variant="custom"
         aria-label="Download"
