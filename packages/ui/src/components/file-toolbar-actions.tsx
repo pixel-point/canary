4d2dedc3355e55306f41acfdd38528afa2915331
Vardan Bansal <vardan.bansal@harness.io>
2025-03-29 13:24:53 -0700

    fix btn group border color (#1344)

--- packages/ui/src/components/file-toolbar-actions.tsx
+++ packages/ui/src/components/file-toolbar-actions.tsx
@@ -1,8 +1,7 @@
 import { FC } from 'react'
 
 import { Button, ButtonGroup, CopyButton, Icon } from '@/components'
-
-const buttonClassName = 'rounded-none border-borders-1 hover:bg-background-4'
+import { cn } from '@utils/cn'
 
 export interface FileToolbarActionsProps {
   onDownloadClick: () => void
@@ -19,10 +18,13 @@ export const FileToolbarActions: FC<FileToolbarActionsProps> = ({
 }) => {
   return (
     <ButtonGroup spacing="0" className="shadow-borders-1 h-full rounded shadow-as-border">
-      <CopyButton className={`${buttonClassName} rounded-l`} name={copyContent} />
+      <CopyButton
+        className={cn('border-borders-1 hover:bg-background-4', showEdit ? 'rounded-l border-r' : 'rounded-l')}
+        name={copyContent}
+      />
       {showEdit && (
         <Button
-          className={`${buttonClassName} border-l`}
+          className="border-borders-1 hover:bg-background-4 border-r"
           size="icon"
           variant="custom"
           aria-label="Edit"
@@ -32,7 +34,7 @@ export const FileToolbarActions: FC<FileToolbarActionsProps> = ({
         </Button>
       )}
       <Button
-        className={`${buttonClassName} border-l rounded-r`}
+        className={cn('border-borders-1 hover:bg-background-4', showEdit ? 'rounded-r' : 'border-l rounded-r')}
         size="icon"
         variant="custom"
         aria-label="Download"
