a16ab62247f24c34ea86c4137bb1fe6854352786
Sanskar <c_sanskar.sehgal@harness.io>
2025-04-10 12:01:26 -0700

    fix: tooltip documentation (#1438)
    * fix: tooltip documentation
    
    * feat: add border arrow
    
    * revert: border
    
    * feat: simplify docs

--- packages/ui/src/components/tooltip.tsx
+++ packages/ui/src/components/tooltip.tsx
@@ -1,6 +1,6 @@
 import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
 
-import { Content, Provider, Root, Trigger } from '@radix-ui/react-tooltip'
+import { Arrow, Content, Provider, Root, Trigger } from '@radix-ui/react-tooltip'
 import { cn } from '@utils/cn'
 
 const TooltipContent = forwardRef<ElementRef<typeof Content>, ComponentPropsWithoutRef<typeof Content>>(
@@ -9,7 +9,7 @@ const TooltipContent = forwardRef<ElementRef<typeof Content>, ComponentPropsWith
       ref={ref}
       sideOffset={sideOffset}
       className={cn(
-        'bg-cn-background-accent text-cn-foreground-1 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-md px-3 py-1.5 text-xs',
+        'bg-cn-background-3 text-cn-foreground-1 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 overflow-hidden rounded-md px-3 py-1.5 text-xs',
         className
       )}
       {...props}
@@ -18,11 +18,19 @@ const TooltipContent = forwardRef<ElementRef<typeof Content>, ComponentPropsWith
 )
 TooltipContent.displayName = Content.displayName
 
+const TooltipArrow = forwardRef<ElementRef<typeof Arrow>, ComponentPropsWithoutRef<typeof Arrow>>(
+  ({ className, ...props }, ref) => (
+    <Arrow ref={ref} width={14} height={8} className={cn('fill-cn-background-3', className)} {...props} />
+  )
+)
+TooltipArrow.displayName = Arrow.displayName
+
 const Tooltip = {
   Root,
   Trigger,
   Content: TooltipContent,
-  Provider
+  Provider,
+  Arrow: TooltipArrow
 }
 
 export { Tooltip }
