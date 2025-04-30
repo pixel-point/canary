de2ea542dee3731360f1117b88e4b170d5eee831
Harish V <164648608+harish-viswa@users.noreply.github.com>
2025-04-30 09:58:18 +0530

    fix: updated connectivity status value for connector-list-page (#1564)

--- packages/ui/src/components/filters/filters-bar/actions/variants/checkbox.tsx
+++ packages/ui/src/components/filters/filters-bar/actions/variants/checkbox.tsx
@@ -17,7 +17,6 @@ const Checkbox = ({ filter, filterOption, onUpdateFilter }: CheckboxFilterProps)
         <div className="px-2 py-1">
           {filteredOptions.map(option => (
             <DropdownMenu.CheckboxItem
-              className="pl-[34px]"
               checked={filterValue.map(v => v.value).includes(option.value)}
               onSelect={event => {
                 event?.preventDefault()
--- packages/ui/src/views/connectors/connectors-list/filter-options.ts
+++ packages/ui/src/views/connectors/connectors-list/filter-options.ts
@@ -5,8 +5,8 @@ import { ConnectorListFilters } from './types'
 
 export const getConnectorListFilterOptions = (t: TFunction): Array<FilterOptionConfig<keyof ConnectorListFilters>> => {
   const options = [
-    { label: t('views:connectors.filterOptions.statusOption.success', 'Success'), value: 'success' },
-    { label: t('views:connectors.filterOptions.statusOption.failure', 'Failed'), value: 'failure' }
+    { label: t('views:connectors.filterOptions.statusOption.success', 'Success'), value: 'SUCCESS' },
+    { label: t('views:connectors.filterOptions.statusOption.failure', 'Failed'), value: 'FAILURE' }
   ]
   return [
     {
