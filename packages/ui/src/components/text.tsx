71cee21e88e13329ac33f3093773e13be9621f31
Andrew Koreykin <ankormoreankor@gmail.com>
2025-05-15 19:50:29 +0400

    migrate Text component to the new design system

--- packages/ui/src/components/text.tsx
+++ packages/ui/src/components/text.tsx
@@ -1,55 +1,71 @@
-import * as React from 'react'
+import { ComponentProps, ElementType } from 'react'
 
 import { Slot } from '@radix-ui/react-slot'
 import { cn } from '@utils/cn'
-import { cva } from 'class-variance-authority'
+import { cva, VariantProps } from 'class-variance-authority'
 
-const textVariants = cva('text-base', {
+type TextElement =
+  | 'span'
+  | 'div'
+  | 'p'
+  | 'h1'
+  | 'h2'
+  | 'h3'
+  | 'h4'
+  | 'h5'
+  | 'h6'
+  | 'label'
+  | 'legend'
+  | 'caption'
+  | 'figcaption'
+  | 'blockquote'
+  | 'pre'
+  | 'kbd'
+  | 'var'
+  | 'code'
+  | 'samp'
+  | 'cite'
+  | 'time'
+  | 'address'
+  | 'strong'
+  | 'abbr'
+  | 'em'
+
+const textVariants = cva('', {
   variants: {
+    variant: {
+      'heading-hero': 'font-heading-hero',
+      'heading-section': 'font-heading-section',
+      'heading-subsection': 'font-heading-subsection',
+      'heading-base': 'font-heading-base',
+      'heading-small': 'font-heading-small',
+      'body-normal': 'font-body-normal',
+      'body-single-line-normal': 'font-body-single-line-normal',
+      'body-strong': 'font-body-strong',
+      'body-single-line-strong': 'font-body-single-line-strong',
+      'body-code': 'font-body-code',
+      'caption-normal': 'font-caption-normal',
+      'caption-soft': 'font-caption-soft',
+      'caption-single-line-normal': 'font-caption-single-line-normal',
+      'caption-single-line-soft': 'font-caption-single-line-soft'
+    },
     align: {
       left: 'text-left',
       center: 'text-center',
       right: 'text-right'
     },
-    size: {
-      0: 'text-1',
-      1: 'text-2',
-      2: 'text-sm',
-      3: 'text-base',
-      4: 'text-xl',
-      5: 'text-2xl',
-      6: 'text-3xl',
-      7: 'text-4xl',
-      8: 'text-5xl',
-      9: 'text-6xl',
-      10: 'text-7xl',
-      11: 'text-8xl',
-      12: 'text-9xl',
-      13: 'text-lg'
-    },
-    trim: {
-      normal: '',
-      start: '',
-      end: '',
-      both: ''
-    },
     truncate: {
       true: 'truncate'
     },
-    weight: {
-      normal: 'font-normal',
-      medium: 'font-medium',
-      semibold: 'font-semibold',
-      bold: 'font-bold'
-    },
     color: {
-      primary: 'text-cn-foreground-1',
-      secondary: 'text-cn-foreground-2',
-      tertiary: 'text-cn-foreground-3',
-      tertiaryBackground: 'text-cn-foreground-3',
       inherit: 'text-inherit',
-      'foreground-4': 'text-cn-foreground-2',
-      'foreground-5': 'text-cn-foreground-3'
+      'foreground-1': 'text-cn-foreground-1',
+      'foreground-2': 'text-cn-foreground-2',
+      'foreground-3': 'text-cn-foreground-3',
+      'state-disabled': 'text-cn-foreground-disabled',
+      success: 'text-cn-foreground-success',
+      warning: 'text-cn-foreground-warning',
+      danger: 'text-cn-foreground-danger'
     },
     wrap: {
       wrap: 'text-wrap',
@@ -57,82 +73,71 @@ const textVariants = cva('text-base', {
       pretty: 'text-pretty',
       balance: 'text-balance'
     }
+  },
+  defaultVariants: {
+    variant: 'body-normal',
+    align: 'left',
+    color: 'foreground-2',
+    truncate: false,
+    wrap: 'wrap'
   }
 })
 
-interface TextProps extends React.ComponentProps<'span'> {
-  /**
-   * Shorthand for changing the default rendered element
-   * into a semantically appropriate alternative.
-   */
-  as?: 'span' | 'div' | 'label' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
-
-  /**
-   * Change the default rendered element for the one
-   * passed as a child, merging their props and behavior.
-   */
-  asChild?: boolean
-
-  /**
-   * Sets the CSS text-align property.
-   */
-  align?: 'left' | 'center' | 'right'
-
-  /**
-   * Sets the text size.
-   */
-  size?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13
-
-  /**
-   * Trims the leading whitespace from the start or end
-   * of the text.
-   */
-  trim?: 'normal' | 'start' | 'end' | 'both'
-
-  /**
-   * Truncates text with an ellipsis.
-   */
-  truncate?: boolean
-
-  /**
-   * Sets the font-weight property.
-   */
-  weight?: 'normal' | 'medium' | 'semibold' | 'bold'
-
-  /**
-   * Sets the color property.
-   */
-  color?: 'primary' | 'secondary' | 'tertiary' | 'tertiaryBackground' | 'foreground-5' | 'foreground-4' | 'inherit'
-
-  /**
-   * Controls the wrapping behavior of the text.
-   */
-  wrap?: 'wrap' | 'nowrap' | 'pretty' | 'balance'
+const textVariantToElement: Record<string, TextElement> = {
+  'heading-hero': 'h1',
+  'heading-section': 'h2',
+  'heading-subsection': 'h3',
+  'heading-base': 'h4',
+  'heading-small': 'h5',
+  'body-code': 'pre'
 }
 
-const Text = ({
+const getTextNode = ({ as, variant, asChild }: Pick<TextProps, 'as' | 'asChild' | 'variant'>) => {
+  if (asChild) {
+    return Slot
+  }
+
+  if (textVariantToElement[variant]) {
+    return textVariantToElement[variant]
+  }
+
+  return (as ?? 'span') as ElementType
+}
+
+type TextProps<E extends TextElement = 'span'> = Omit<ComponentProps<E>, 'color'> &
+  Omit<VariantProps<typeof textVariants>, 'variant'> & {
+    variant: Exclude<VariantProps<typeof textVariants>['variant'], undefined | null>
+    /**
+     * Shorthand for changing the default rendered element
+     * into a semantically appropriate alternative.
+     */
+    as?: TextElement
+    /**
+     * Change the default rendered element for the one
+     * passed as a child, merging their props and behavior.
+     */
+    asChild?: boolean
+  }
+
+const Text = <E extends TextElement = 'span'>({
   className,
   children,
+  variant,
   as,
   asChild,
   align,
-  size = 2,
-  weight = 'normal',
-  color = 'primary',
-  trim,
+  color,
   truncate,
   wrap,
   ...props
-}: TextProps) => {
-  const Comp = asChild ? Slot : ((as ? as.toString() : 'span') as any)
+}: TextProps<E>) => {
+  const Comp = getTextNode({ as, variant, asChild })
 
   return (
-    <Comp className={cn(textVariants({ align, size, weight, color, trim, truncate, wrap }), className)} {...props}>
+    <Comp className={cn(textVariants({ variant, align, color, truncate, wrap }), className)} {...props}>
       {children}
     </Comp>
   )
 }
-
-Text.displayName = 'Text'
 
 export { Text }
